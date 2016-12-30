4f7b8f7c4bf5f37ac571b81ab49a091a83d7fd34
Qing <qing.sheng@thomsonreuters.com>
2016-12-30 12:08:19 +0800

    add

---
+++ CSConsoleTest/CSConsoleTest/DateTimeTests/Format.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateTimeTests
+{
+    class Format
+    {
+        public void TestFormat()
+        {
+            var d = System.DateTime.Now;
+            Console.WriteLine(d.ToString("dd-MMM-yyyy hh:mm:ss.ff tt"));
+            Console.WriteLine(d.ToString("dd-MMM-yyyy hh:mm:ss.ffff tt"));
+            Console.WriteLine(d.ToString("dd-MMM-yyyy hh:mm:ss.ffffff tt"));
+            Console.WriteLine(d.ToString("dd-MMM-yyyy hh:mm:ss.fffffff tt"));
+        }
+    }
+}
